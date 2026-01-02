08fb41e25071de0648fba08259284328930bf1fe
talrasha007 <talrasha007@gmail.om>
2026-01-02 08:53:43 +0800

    parse.

--- src/pages/api/pb/[account].ts
+++ src/pages/api/pb/[account].ts
@@ -1,5 +1,18 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ params: { account }, url, locals: { runtime } }) => {
+  const { orderNum, sAmount: sAmountStr, eCommis: eCommisStr, cTime, pTime, region, device, tracking } = Object.fromEntries(url.searchParams.entries());
+  if (orderNum && sAmountStr && eCommisStr && cTime && pTime && region && device && tracking) {
+    const sAmount = parseFloat(sAmountStr);
+    const eCommis = parseFloat(eCommisStr);
+    if (isNaN(sAmount) || isNaN(eCommis)) {
+      return Response.json({ status: 'error', message: 'Invalid amount format' }, { status: 400 });
+    }
+
+    if (tracking.indexOf(`|${account}|`) === -1) {
+      return Response.json({ status: 'error', message: 'Tracking does not match account' }, { status: 400 });
+    }
+  }
+  
   return Response.json({ status: 'ok' });
 };
