82d9492f1c5cfb807e3f552288a417e196d04bbd
Tal <talrasha007@gmail.com>
2026-01-24 15:41:40 +0800

    reuse session.

--- src/pages/api/browser.ts
+++ src/pages/api/browser.ts
@@ -1,4 +1,5 @@
 import puppeteer from '@cloudflare/puppeteer';
+import type { Browser, BrowserWorker } from '@cloudflare/puppeteer';
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ url, locals: { runtime: { env: { MYBROWSER } } } }) => {
@@ -7,7 +8,7 @@ export const GET: APIRoute = async ({ url, locals: { runtime: { env: { MYBROWSER
   const proxyUsername = url.searchParams.get('proxy-user') || undefined;
   const proxyPassword = url.searchParams.get('proxy-pass') || undefined;
 
-  const browser  = await puppeteer.launch(MYBROWSER);
+  const browser  = await connectToBrowserWorker(MYBROWSER);
   const context = await browser.createBrowserContext({ proxyServer });
   const page = await context.newPage();
 
@@ -17,11 +18,35 @@ export const GET: APIRoute = async ({ url, locals: { runtime: { env: { MYBROWSER
       password: proxyPassword,
     });
   }
-  
+
   await page.goto(uri);
   // const metrics = await page.metrics();
   const html = await page.evaluate(() => document.documentElement.innerHTML);
   await browser.close();
 
   return new Response(html);
+}
+
+async function connectToBrowserWorker(endpoint: BrowserWorker) {
+  return await puppeteer.connect(endpoint, await getRandomSession(endpoint)).catch(async () => await puppeteer.launch(endpoint));
+}
+
+// Pick random free session
+// Other custom logic could be used instead
+async function getRandomSession(endpoint: BrowserWorker) {
+  const sessions = await puppeteer.sessions(endpoint);
+  const sessionsIds = sessions
+    .filter((v) => {
+      return !v.connectionId; // remove sessions with workers connected to them
+    })
+    .map((v) => {
+      return v.sessionId;
+    });
+  if (sessionsIds.length === 0) {
+    return;
+  }
+
+  const sessionId = sessionsIds[Math.floor(Math.random() * sessionsIds.length)];
+
+  return sessionId;
 }
