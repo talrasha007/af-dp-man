36dc85cdc1e9cc01812456948b46069208f5bcfe
Tal <talrasha007@gmail.com>
2026-01-29 20:05:10 +0800

    parse url.

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,8 +1,16 @@
 import type { APIRoute } from 'astro';
 
+function parseProxy(proxy: string) {
+  const match = proxy.match(/^(https?:\/\/)([^:]+):([^@]+)@(.+)$/);
+  if (!match) {
+    return { url: proxy };
+  }
+  const [, protocol, username, password, host] = match;
+  return { username, password, url: `${protocol}${host}` };
+}
+
 export const GET: APIRoute = async ({ url, locals: { runtime: { env: { PB_DB } } } }) => {
   const actived = url.searchParams.get('actived');
-  console.log(actived);
 
   const sql = 'SELECT * FROM tasks';
   const tasks = await PB_DB.prepare(sql).all();
@@ -13,7 +21,12 @@ export const GET: APIRoute = async ({ url, locals: { runtime: { env: { PB_DB } }
       .map(task => {
         delete task.icon_url;
         delete task.disabled;
-        return { ...task, countries: JSON.parse(task.countries as string).join(','), disabled: !!task.disabled };
+        return { ...task,
+          countries: JSON.parse(task.countries as string).join(','),
+          proxy: parseProxy(task.proxy as string),
+          send_page_view: !!task.send_page_view,
+          use_page_view: !!task.use_page_view,
+        };
       });
     return Response.json(results);
   } else {
