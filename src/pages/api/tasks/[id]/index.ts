a6154b3a86191debbb060d0a0f163b7f004a30b7
Tal <talrasha007@gmail.com>
2026-01-29 16:07:16 +0800

    task item.s

---
+++ src/pages/api/tasks/[id]/index.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ params, locals: { runtime: { env: { PB_DB } } } }) => {
+  const taskSql = 'SELECT * FROM tasks WHERE id = ?';
+  const task = await PB_DB.prepare(taskSql).bind(params.id).first();
+  if (!task) {
+    return new Response('Task not found', { status: 404 });
+  }
+
+  const itemsSql = 'SELECT * FROM task_items WHERE task_id = ?';
+  const taskItems = await PB_DB.prepare(itemsSql).bind(params.id).all();
+  return Response.json({ task, items: taskItems.results });
+}
