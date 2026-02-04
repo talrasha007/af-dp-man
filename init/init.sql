27f38d59caddec74702b54684a63c9d96fbb269e
Tal <talrasha007@gmail.com>
2026-02-04 18:48:38 +0800

    short.

--- init/init.sql
+++ init/init.sql
@@ -91,6 +91,8 @@ CREATE TABLE apps (
 );
 
 INSERT INTO apps (app_id, app_name, dev_key) VALUES
+    ('id6514312894', 'Flickreels', 'B2Si3mnTrbSUYD3wKgQyrT'),
+    ('id1636235979', 'Reelshort', 'YMqy4FA8K8AHjhXuzyRoQg'),
     ('id1034231507', 'blibli', 'tHu6W8t9EpSvifDatvX7V'),
     ('id1359763701', 'Exness', 'trLtuVrbebvUhUPUhCJAXg'),
     ('com.dhgate.buyermob', 'DHgate', 'wAueXqnTbMD65NFK9TsKS7'),
