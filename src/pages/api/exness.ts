5aa6498f7acd76721d344b5831531962d9ac1fe2
Tal <talrasha007@gmail.com>
2026-03-09 18:49:04 +0800

    ifa

--- src/pages/api/exness.ts
+++ src/pages/api/exness.ts
@@ -2,15 +2,30 @@ import type { APIRoute } from 'astro';
 
 const originUrl = 'https://exness.onelink.me/ktkL/0j5yyo15?_8f4z=1&ex_ol=1&track_uid=749d98b0-848f-4863-96aa-afdb4b7ac40d&agent=c_h1yd65dg9x&agent_full_path=%2Fa%2Fc_h1yd65dg9x%3F_8f4z%3D1%26platform%3Dmobile%26ex_ol%3D1&agent_platform=mt4&agent_timestamp=1772809784225&af_sub3=c_h1yd65dg9x&af_sub4=749d98b0-848f-4863-96aa-afdb4b7ac40d&af_sub5=eyJhZ2VudF9mdWxsX3BhdGgiOiAiL2EvY19oMXlkNjVkZzl4P184ZjR6PTEmcGxhdGZvcm09bW9iaWxlJmV4X29sPTEiLCAiYWdlbnRfcGxhdGZvcm0iOiAibXQ0IiwgImFnZW50X3RpbWVzdGFtcCI6IDE3NzI4MDk3ODQyMjV9';
 
+function isIos(ua: string) {
+  return /iPhone|iPad|iPod/i.test(ua);
+}
+
 export const GET: APIRoute = async ({ request: { headers }, url: { searchParams } }) => {
+  const ua = searchParams.get('ua') || headers.get('user-agent') || '';
   const mapToAf = searchParams.get('map_to_af') === 'true';
 
   const url = new URL(originUrl);
+
+  const ifa = searchParams.get('ifa');
+  if (ifa) {
+    if (isIos(ua)) {
+      url.searchParams.set('idfa', ifa);      
+    } else {
+      url.searchParams.set('advertising_id', ifa);
+    }
+  }
+
   if (mapToAf) {
     url.hostname = 'app.appsflyer.com';
 
     const ua = headers.get('user-agent') || '';
-    if (ua.includes('iPhone') || ua.includes('iPad')) {
+    if (isIos(ua)) {
       url.pathname = '/id1359763701';
     } else {
       url.pathname = '/com.exness.android.pa';
