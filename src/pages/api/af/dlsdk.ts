7b0480623c278dfe20a71502e70def830564ef16
Tal <talrasha007@gmail.com>
2026-08-19 21:14:37 +0800

    no forward.

--- src/pages/api/af/dlsdk.ts
+++ src/pages/api/af/dlsdk.ts
@@ -46,7 +46,7 @@ export const GET: APIRoute = async ({ url, locals: { runtime: { env: { PB_DB } }
       'Accept': 'application/json',
       'Content-Type': 'application/json; charset=utf-8',
       'User-Agent': isIos ? `AppsFlyerSDK/6.17 (iPhone; iOS ${version || '26.1'}; Scale/3.00)` : `AppsFlyerSDK/6.17 (Android; Android ${version || '13'}; Scale/3.00)`,
-      ...(ip ? { 'X-Forwarded-For': ip } : {}),
+      // ...(ip ? { 'X-Forwarded-For': ip } : {}),
     },
   });
 }
